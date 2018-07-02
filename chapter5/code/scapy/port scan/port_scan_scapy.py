9b5b6677bc10cfcb4cbfbb641ffb1af2b642da37
jmortega <jmoc25@gmail.com>
2018-07-02 22:30:45 +0200

    Update port_scan_scapy.py

--- chapter5/code/scapy/port scan/port_scan_scapy.py
+++ chapter5/code/scapy/port scan/port_scan_scapy.py
@@ -1,5 +1,6 @@
 from scapy.all import sr1, IP, TCP
 
+OPEN_PORTS = []
 
 def analyze_port(host, port):
 	"""
@@ -8,19 +9,19 @@ def analyze_port(host, port):
     :param port: port to test
     :type port: int
 	"""
-    OPEN_PORTS = []
+  
     print "[ii] Scanning port %s" % port
     res = sr1(IP(dst=host)/TCP(dport=port), verbose=False, timeout=0.2)
-        if res is not None and TCP in res:
-            if res[TCP].flags == 18:
-                OPEN_PORTS.append(port)
-                print "Port  %s open " % port
+    if res is not None and TCP in res:
+		if res[TCP].flags == 18:
+                	OPEN_PORTS.append(port)
+                	print "Port  %s open " % port
 
 
 def main():
     for x in xrange(0, 80):
         analyze_port("domain", x)
-            print "[*] Ports openned:"
+    print "[*] Ports openned:"
     for x in OPEN_PORTS:
         print " - %s/TCP" % x
         
