07566ce2caefe549c9c4d17ce86765d4f3e0532e
Ortega Candel <jose-manuel.ortega-candel@capgemini.com>
2018-07-30 19:50:32 +0200

    chapter 7 code and images

--- chapter7/code/paramiko/ssh_command_client.py
+++ chapter7/code/paramiko/ssh_command_client.py
@@ -4,7 +4,6 @@ import subprocess
 
 def ssh_command(ip, user, passwd, command):
     client = paramiko.SSHClient()
-    #client.load_host_keys('/home/user/.ssh/known_hosts')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=user, password=passwd)
     ssh_session = client.get_transport().open_session()
@@ -19,6 +18,4 @@ def ssh_command(ip, user, passwd, command):
             except Exception,e:
                 ssh_session.send(str(e))
         client.close()
-    return    
-
-ssh_command('192.168.56.101', 'msfadmin', 'msfadmin','ls -la')
+    return
