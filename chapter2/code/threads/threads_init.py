96b936972a625b7366ee9976ee20eb19a41c56c9
Ortega Candel <jose-manuel.ortega-candel@capgemini.com>
2018-08-20 17:24:23 +0200

    images and code chapter 2

--- chapter2/code/threads/threads_init.py
+++ chapter2/code/threads/threads_init.py
@@ -1,11 +1,15 @@
 import thread
 import time
 
+num_threads = 4
+
 def thread_message(message):
+	global num_threads
+	num_threads -= 1
 	print('Message from thread %s\n' %message)
 
-thread.start_new_thread(thread_message,("I am the first thread",))
-thread.start_new_thread(thread_message,("I am the second thread",))
-thread.start_new_thread(thread_message,("I am the third thread",))
+while num_threads > 0:
+	print "I am the %s thread" %num_threads
+	thread.start_new_thread(thread_message,("I am the %s thread" %num_threads,))
+	time.sleep(0.1)
 
-time.sleep(0.1)
